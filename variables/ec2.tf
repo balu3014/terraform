141f15c02d61a129b219abae92dc7b1056f222f3
balu3014 <161106157+balu3014@users.noreply.github.com>
2024-06-17 23:01:17 +0530

    terraform

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -1,32 +1,30 @@
 # resource <resource-type> <resource-name>
 resource "aws_instance" "db" {
 
-  ami                    = "ami-0b4f379183e5706b9"
+  ami                    = var.image_id
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-  instance_type          = "t2.micro"
-
-  tags = {
-    Name = "db"
-  }
+  instance_type          = var.instance_type
+  tags                   = var.tags
+  
 }
 
 resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "allowing SSH access"
+  name        = var.sg_name
+  description = "var.sg_description"
 
   #terraform block
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = var.ssh_port
+    to_port     = var.ssh_port
+    protocol    = var.protocol
+    cidr_blocks = var.allowed_cidr
   }
 
   egress {
     from_port   = 0 # from 0 to 0 means, opening all protocols
     to_port     = 0
     protocol    = "-1" # -1 all protocols
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = var.allowed_cidr
   }
 
   tags = {
---
+++ variables/variables.tf
@@ -0,0 +1,44 @@
+#1. command line
+#2. tfvars
+#3. default values
+variable "image_id" {
+  type    = string
+  default = "ami-0b4f379183e5706b9"
+  description = "RHEL-9 AMI ID"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "tags" {
+    default = {
+        project = "expense"
+        Environment = "Dev"
+        Module = "DB"
+        Name = "db"
+    }
+}
+
+variable "sg_name" {
+     default = "allow_ssh"
+}
+
+variable "sg_description" {
+    default = "allowing port 22"
+}
+
+
+variable "ssh_port" {
+    default = 22
+}
+
+variable "protocol" {
+    default = "tcp"
+}
+
+variable "allowed_cidr" {
+    type = list(string)
+    default = ["0.0.0.0/0"]
+}
