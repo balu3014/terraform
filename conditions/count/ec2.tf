bab6bb00c7074c44ebcff98e68117c4ea86f5e27
balu3014 <161106157+balu3014@users.noreply.github.com>
2024-06-18 17:45:53 +0530

    terraform

---
+++ conditions/count/ec2.tf
@@ -0,0 +1,37 @@
+# resource <resource-type> <resource-name>
+resource "aws_instance" "db_backend_frontend" {
+  count                  = 3 # This will create 3 instances
+  ami                    = "ami-0b4f379183e5706b9"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  instance_type          = "t2.micro"
+
+  tags = {
+    Name = var.instance_names[count.index]
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "allowing SSH access"
+
+  #terraform block
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0 # from 0 to 0 means, opening all protocols
+    to_port     = 0
+    protocol    = "-1" # -1 all protocols
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name      = "allow_ssh"
+    CreatedBy = "Balu Thripurari"
+  }
+}
+
