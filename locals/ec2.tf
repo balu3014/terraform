c70b247efb03704f130c1a5ae36fb2570be21826
balu3014 <161106157+balu3014@users.noreply.github.com>
2024-08-15 11:18:46 +0530

    all

---
+++ locals/ec2.tf
@@ -0,0 +1,12 @@
+# resource <resource-type> <resource-name>
+   
+   
+resource "aws_instance" "expense" {
+
+  ami  = local.ami_id
+  vpc_security_group_ids = [local.sg_id]
+  instance_type          = local.instance_type
+
+  tags = local.tags
+
+}
---
+++ locals/locals.tf
@@ -0,0 +1,11 @@
+locals {
+  
+
+  ami_id = "ami-041e2ea9402c46c32"
+  sg_id = "sg-02433cc121db80627"
+  instance_type = "t2.micro"
+  tags ={
+     Name = "locals"
+    CreatedBy = "Balu Thripurari"
+  }
+}
