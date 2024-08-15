057016aa7e5cd2f5b5136bdeadb2154f08ab69d8
balu3014 <161106157+balu3014@users.noreply.github.com>
2024-08-15 11:31:55 +0530

    all

--- locals/locals.tf
+++ locals/locals.tf
@@ -1,9 +1,14 @@
+
+# locals are like variables but Locals can hold expressions we can use where ever we want 
+
+
 locals {
   
 
   ami_id = "ami-041e2ea9402c46c32"
   sg_id = "sg-02433cc121db80627"
-  instance_type = "t2.micro"
+  #instance_type = "t2.micro"
+  instance_type = var.instance_name == "db" ? "t3.small" : "t3.micro"
   tags ={
      Name = "locals"
     CreatedBy = "Balu Thripurari"
