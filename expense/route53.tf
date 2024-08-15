dbb9455b6faf12409ece8c9d2e58b2af3ed5aaa0
balu3014 <161106157+balu3014@users.noreply.github.com>
2024-08-15 12:41:38 +0530

    all

--- expense/route53.tf
+++ expense/route53.tf
@@ -2,8 +2,10 @@ resource "aws_route53_record" "expense" {
     count = length(var.instance_names)
   zone_id = var.zone_id
   name    = var.instance_names[count.index] == "frontend" ? var.domain_name : "${var.instance_names[count.index]}.${var.domain_name}" # use terraform interpolation
+  #name = local.name
   type    = "A"
   ttl     = 300
+  #records = local.records
   records = [aws_instance.expense[count.index]] == "frontend" ? [aws_instance.expense[count.index].public_ip] : [aws_instance.expense[count.index].private_ip]
 
 #if records already exist
