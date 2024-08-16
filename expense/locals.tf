b1e7e4d9f0cc1ee2bbcb25185d7a665eac9c7fc2
balu3014 <161106157+balu3014@users.noreply.github.com>
2024-08-16 15:56:56 +0530

    all

--- expense/locals.tf
+++ expense/locals.tf
@@ -1,4 +1,5 @@
-# locals {
+# count & count.index will not work in Locals
+#locals {
 #  name = var.instance_names[count.index] == "frontend" ? var.domain_name : "${var.instance_names[count.index]}.${var.domain_name}"
 #  records = [aws_instance.expense[count.index]] == "frontend" ? [aws_instance.expense[count.index].public_ip] : [aws_instance.expense[count.index].private_ip]
 # }
